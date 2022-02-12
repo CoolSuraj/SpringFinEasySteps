83d6fbd1ed990e352c95add437600908335ac3f7
sipher <sipher@kali>
2022-02-12 09:02:43 +0530

    set injection

---
+++ springcore/src/main/java/com/suraj/spring/course/springcore/set/setTest.java
@@ -0,0 +1,16 @@
+package com.suraj.spring.course.springcore.set;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class setTest {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/suraj/spring/course/springcore/set/setConfig.xml");
+		CarDealer bean = (CarDealer) context.getBean("CarDealer"); //setting the bean object
+		System.out.println(bean.getName());
+		System.out.println(bean.getModels());  //injected set Wohoooo!!!
+		
+	}
+
+}
