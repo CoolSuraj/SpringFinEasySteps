0a2e5bea67c0c4e7004245ae640b3b4a2c9eae12
root <root@kali>
2022-06-04 17:49:03 +0530

    changes for config

---
+++ springcore/src/main/java/com/suraj/spring/course/lifecycle/xmlconfig/Patient.java
@@ -0,0 +1,15 @@
+package com.suraj.spring.course.lifecycle.xmlconfig;
+//poja
+public class Patient {
+	int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+
+}
