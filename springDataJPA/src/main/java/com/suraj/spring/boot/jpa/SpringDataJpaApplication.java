d1ac77c4cad8859c43c8ebc384e66fb1179222d2
root <root@kali>
2022-06-05 22:34:01 +0530

    Additional JPA operations

--- springDataJPA/src/main/java/com/suraj/spring/boot/jpa/SpringDataJpaApplication.java
+++ springDataJPA/src/main/java/com/suraj/spring/boot/jpa/SpringDataJpaApplication.java
@@ -7,9 +7,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages={"com.suraj.spring.*"})//this is to be written so that it will scan respective packages
 @EnableJpaRepositories(basePackages ={"com.suraj.spring.*"}) //Imp for JPA
-@EntityScan("com.suraj.spring.*") //Imp for Scanning 
+@EntityScan("com.suraj.spring.*") //Imp for Scanning entities not used earlier but now necessary
 public class SpringDataJpaApplication {
-
+	// for starters we have taken care of things in Tests file so please look there 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataJpaApplication.class, args);
 	}
